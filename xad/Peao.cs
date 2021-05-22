f0d927949fd84c0ebfed138acc08a27631dce22c
Coletinn <gustavocoleto@hotmail.com>
2021-05-22 16:52:56 -0300

    Correcao - metodo existeInimigo da classe Peao

--- xad/Peao.cs
+++ xad/Peao.cs
@@ -22,7 +22,7 @@ namespace xad
         public bool existeInimigo(Posicao pos)
         {
             Peca p = tab.peca(pos);
-            return p != null || p.cor != cor;
+            return p == null || p.cor != cor;
         }
 
         private bool livre(Posicao pos)
