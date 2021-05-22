4664c95909eb312f5eaa0572997cdff559da3815
Coletinn <gustavocoleto@hotmail.com>
2021-05-22 16:06:48 -0300

    Jogada especial roque pequeno e roque grande

--- xad/PartidaDeXadrez.cs
+++ xad/PartidaDeXadrez.cs
@@ -38,6 +38,26 @@ namespace xad
             {
                 capturadas.Add(pecaCapturada);
             }
+
+            // JOGADA ESPECIAL - ROQUE PEQUENO
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origemTorre);
+                T.incrementarQtdMovimentos();
+                tab.colocarPeca(T, destinoTorre);
+            }
+
+            // JOGADA ESPECIAL - ROQUE GRANDE
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemTorre);
+                T.incrementarQtdMovimentos();
+                tab.colocarPeca(T, destinoTorre);
+            }
             return pecaCapturada;
         }
 
@@ -52,6 +72,26 @@ namespace xad
                 capturadas.Remove(pecaCapturada);
             }
             tab.colocarPeca(p, origem);
+
+            // JOGADA ESPECIAL - ROQUE PEQUENO
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoTorre);
+                T.decrementarQtdMovimentos();
+                tab.colocarPeca(T, origemTorre);
+            }
+
+            // JOGADA ESPECIAL - ROQUE GRANDE
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(destinoTorre);
+                T.decrementarQtdMovimentos();
+                tab.colocarPeca(T, origemTorre);
+            }
         }
 
 
@@ -245,7 +285,7 @@ namespace xad
             colocarNovaPeca('b', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('c', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('d', 1, new Dama(tab, Cor.Branca));
-            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca));
+            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca, this));
             colocarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
@@ -262,7 +302,7 @@ namespace xad
             colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('c', 8, new Bispo(tab, Cor.Preta));
             colocarNovaPeca('d', 8, new Dama(tab, Cor.Preta));
-            colocarNovaPeca('e', 8, new Rei(tab, Cor.Preta));
+            colocarNovaPeca('e', 8, new Rei(tab, Cor.Preta, this));
             colocarNovaPeca('f', 8, new Bispo(tab, Cor.Preta));
             colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('h', 8, new Torre(tab, Cor.Preta));
--- xad/Rei.cs
+++ xad/Rei.cs
@@ -7,9 +7,12 @@ namespace xad
 {
     class Rei : Peca
     {
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
-        {
 
+        private PartidaDeXadrez partida;
+
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
+        {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -21,6 +24,13 @@ namespace xad
         {
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor;
+        }
+
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p is Torre && p.cor == cor && p.qtdMovimentos == 0;
         }
 
         public override bool[,] movimentosPossiveis()
@@ -77,6 +87,39 @@ namespace xad
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            // JOGADA ESPECIAL - ROQUE 
+            if (qtdMovimentos == 0 && !partida.xeque)
+            {
+                // ROQUE PEQUENO
+                Posicao posTorre1 = new Posicao(posicao.linha, pos.coluna + 3);
+                if (testeTorreParaRoque(posTorre1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+                // ROQUE GRANDE
+                Posicao posTorre2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorreParaRoque(posTorre2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+            }
+
+            
+
+
             return mat;
         }
     }
